048dcb5ebb8332b873b9250b4aa4f8d59d8204f4
Hiren J Patel <127721115+Hiren20599@users.noreply.github.com>
2023-03-31 11:30:09 +0530

    Add files via upload

---
+++ intro.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+//Display This Information using printf a. Your Name b. Your Birth date c. Your Age d. Your address
+
+void main()
+
+{
+	printf("Name:- Hiren Jitendrabhai Patel\n");
+	printf("Birth Date:- 20/05/1999\n");
+	printf("Age:- 23\n");
+	printf("Address:- B/303, Shree Krishna Complex, Jahangirpura-Olpad Road, Surat\n");
+}
+
---
+++ square-cube.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+//Write a program to make a square and cube of number.
+
+int main()
+
+{
+	int a,b,c;
+
+	printf("Enter the number: ");
+	scanf("%d", &a);
+	
+	b=a*a;
+	
+	printf("Square of given number is: %d\n", b);
+	
+	c=a*a*a;
+	
+	printf("Cube of given number is: %d\n", c);
+}
